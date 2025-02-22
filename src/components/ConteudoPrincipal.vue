5303fa82d9ce1dbec886420ff816c7072df2a820
edson.ribeiro_onfly <edson.ribeiro@onfly.com.br>
2025-02-21 21:55:23 -0300

    vue

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,8 +1,9 @@
 <script lang="ts">
 import SelecionarIngredientes from "@/components/SelecionarIngredientes.vue";
+import Tag from "@/components/Tag.vue";
 
 export default {
-  components: {SelecionarIngredientes},
+  components: {Tag, SelecionarIngredientes},
   data() {
     return {
       ingredientes: ['Alho', 'Manreiga', 'Orégano']
@@ -20,7 +21,7 @@ export default {
         </span>
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
         <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
-          {{ingrediente}}
+          <Tag texto="ingrediente" ativa/>
         </li>
       </ul>
       <p v-else class="paragrafo lista-vazia">
@@ -59,18 +60,6 @@ export default {
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-  transition: 0.2s;
-  color: var(--creme, #FFFAF3);
-  background: var(--coral, #F0633C);
-  font-weight: 700;
 }
 
 .lista-vazia {
